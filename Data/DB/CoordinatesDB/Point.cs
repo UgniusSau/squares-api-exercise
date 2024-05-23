eb5e75198a92e192e3f154d3017e3ea336d4dabb
Ugnius Saulevičius <ugnius.saulevicius@gmail.com>
2024-05-23 21:52:09 +0300

    chore: cleanup

--- Data/DB/CoordinatesDB/Point.cs
+++ Data/DB/CoordinatesDB/Point.cs
@@ -14,11 +14,6 @@ namespace Data.DB.CoordinatesDB
         public int X { get; set; }
         public int Y { get; set; }
 
-        //public Point(int X, int Y)
-        //{
-        //    this.X = X;
-        //    this.Y = Y;
-        //}
 
         public override bool Equals(object? obj)
         {
